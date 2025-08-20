a50aefabd34ef94061ad4c68e34a08c529714698
CroquetaConQueso <lacio161@gmail.com>
2025-08-20 19:24:52 +0200

    Adding another custom anotation, this time for the surname of the user

--- src/main/java/com/pepito/jose/validators/ApellidoValidador.java
+++ src/main/java/com/pepito/jose/validators/ApellidoValidador.java
@@ -3,8 +3,10 @@ package com.pepito.jose.validators;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-public class ApellidoValidador implements ConstraintValidator<PasswordValida,String>{
+public class ApellidoValidador implements ConstraintValidator<ApellidoValido,String>{
 
+    @Override
+    public void initialize(ApellidoValido wawa){}
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
         if(value == null || value.isBlank() || value.isEmpty()){
